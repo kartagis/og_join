c018c9f311c1d1f1e6b20b53d81703367d179bc0
Kartagis <tolga@ozses.net>
2020-10-07 14:13:56 +0300

    Used dependency injection

--- src/Plugin/Block/OgJoinBlock.php
+++ src/Plugin/Block/OgJoinBlock.php
@@ -1,28 +1,55 @@
 <?php
 
-namespace Drupal\og_join\Plugin\Block;
+namespace Drupal\example\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormstateInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a OgJoin block
- * 
- * @Block (
- *  id = "og_join_block",
+ * Provides a 'ExampleFormBlock' block.
+ *
+ * @Block(
+ *  id = "oj_join_block",
  *  admin_label = @Translation("OgJoin"),
- *  category = @Translation("OgJoin")
  * )
  */
-class OgJoinBlock extends BlockBase {
+class ExampleFormBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  protected $formBuilder;
 
-    /**
-     * {@inheritDoc}
-     */
-    public function build() {
-        $form = \Drupal::formBuilder()->getForm('\Drupal\og_join\Form\OgJoinForm');
-        return [
-            '#markup' => \Drupal::service('renderer')->render($form)
-        ];
-    }
+  /**
+   * @param array $configuration
+   * @param [type] $plugin_id
+   * @param [type] $plugin_definition
+   * @param FormBuilderInterface $formBuilder
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * @param ContainerInterface $container
+   * @param array $configuration
+   * @param [type] $plugin_id
+   * @param [type] $plugin_definition
+   * @return void
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * @return void
+   */
+  public function build() {
+    $form = $this->formBuilder->getForm('\Drupal\og_join\Form\OgJoinForm');
+    return $form;
+  }
 }
